783ac54d74257e45d669e6eded8279e2d08bd53b
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-09-24 01:51:00 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'Widgets',
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "Widgets",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), postcss({ inject: true, minimize: true })],
 });
