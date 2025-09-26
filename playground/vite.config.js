dfafe5d28adb017f1cb531732002d6e07f2cc1ca
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-09-26 10:43:41 +0530

    changes

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@anarock/widgets': path.resolve(__dirname, '../src')
-    }
-  }
-})
+      "@anarock/widgets": path.resolve(__dirname, "../src"),
+    },
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
