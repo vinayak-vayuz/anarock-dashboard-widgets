e030d6a21ddd3d4c8d25243eb8a8ac0c4953375e
ayushchrs03 <ayush.chaurasia@vayuz.com>
2026-03-13 17:43:28 +0530

    fix chart for the booking Chart

--- src/widgets/gate-and-security/component/StaffAttendance.jsx
+++ src/widgets/gate-and-security/component/StaffAttendance.jsx
@@ -31,7 +31,7 @@ function StaffAttendance({ staffPresentAbsent, staffPresentByRole }) {
     >
       <div>
         {/* Top Attendance Summary */}
-        <div className="flex gap-[24px] text-center">
+        <div className="flex gap-[16px] text-center  mt-[24px]">
           <div className="flex-1 py-[12px] bg-[#F7FEFA] rounded h-[76px]">
             <div className="text-[24px] font-medium text-[#1FA05B]">
               {presentStaff ?? 0}
