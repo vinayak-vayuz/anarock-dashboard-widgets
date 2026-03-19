bdeb398253c88b31506f134b5b7767c4810b9d78
priyanshu <priyanshu.singh@vayuz.com>
2026-03-19 14:49:00 +0530

    live

--- src/widgets/components/Card.jsx
+++ src/widgets/components/Card.jsx
@@ -18,7 +18,7 @@ const Card = ({
       className={`relative bg-white !border-[0.5px] !border-[#EBEBEB] !rounded-[12px] !shadow-[0_0_12px_0_#EAF2FF] p-[16px] flex flex-col gap-[16px] ${className}`}
     >
       <div className="flex items-center justify-between">
-        <div className={`flex items-center gap-[12px] ${iconClassName}`}>
+        <div className={`flex items-center gap-[8px] ${iconClassName}`}>
           {icon}
           <div
             style={{
